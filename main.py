7e4d5a368598eb9ffe05b3b1b0c2916ca91f6439
Vedansh <me.vedanshwani@gmail.com>
2025-06-01 13:31:14 +0530

    remdered the initial black screen using pgame

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame;
+from constants import * ;
 
 def main():
     print('Starting Asteroids!');
+    print(f"Screen width: {SCREEN_WIDTH}");
+    print(f"Screen height: {SCREEN_HEIGHT}");
+
+    pygame.init();
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT));
+
+    # game loop
+    while(1):
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                return;
+    
+        screen.fill((0,0,0));
+        pygame.display.flip();
+
 
 if __name__ == "__main__":
     main();
